0be055c6d054930fd87530073433c425081195b4
yoleo@yandex.ru <yoleo@yandex.ru>
2021-10-05 09:58:02 +0200

    Small refactor

--- src/components/organisms/Tree.svelte
+++ src/components/organisms/Tree.svelte
@@ -2,7 +2,6 @@
   import type { Tree } from '../../types/common'
   import type { ThemeContext } from '../../components/context/context'
   import type { ItemGroups } from '../../codegen'
-
   import { getContext } from 'svelte'
 
   let { theme } = getContext<ThemeContext>('theme')
@@ -10,24 +9,30 @@
   export let selectedID: number = 0
   export let tree: Tree<ItemGroups>
   export let onClick: (branch: Tree<ItemGroups>) => void = (tree) => {}
-  // HOW to handle click in recursive components? =) Zzz...
+  // HOW to handle click in recursive components? works only parrent... =) Zzz...
 </script>
 
-<ul
-  style={`padding-left: calc(${$theme.paddings.quarter} * ${tree.depth + 1});${
-    Number(selectedID) === Number(tree.id)
-      ? `background: ${$theme.palette.mainAccentBackground};`
-      : ''
-  }`}
-  class="tree"
->
+{#if tree.children}
+  <ul
+    style={`padding-left: calc(${$theme.paddings.quarter} * ${
+      tree.depth + 1
+    });${
+      Number(selectedID) === Number(tree.id)
+        ? `background: ${$theme.palette.mainAccentBackground};`
+        : ''
+    }`}
+    class="tree"
+  >
+    <li>{tree.data.name}</li>
+    {#if tree.children}
+      {#each tree.children as el}
+        <svelte:self tree={el} />
+      {/each}
+    {/if}
+  </ul>
+{:else}
   <li>{tree.data.name}</li>
-  {#if tree.children}
-    {#each tree.children as el}
-      <svelte:self tree={el} />
-    {/each}
-  {/if}
-</ul>
+{/if}
 
 <style>
   .tree {
