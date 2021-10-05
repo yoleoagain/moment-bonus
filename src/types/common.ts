e8eac605a5928107773aed0c554b3b338b042999
yoleo@yandex.ru <yoleo@yandex.ru>
2021-10-05 09:58:08 +0200

    file

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,16 +1,14 @@
 ﻿export interface StringHashMap {
   [key: string]: string
 }
-
 export interface NumberHashMap {
   [key: string]: number
 }
-
 export interface Tree<T>{
   children: null | Tree<T>[]
   data: T
   parent: null | T
   depth: 0
-  id: number //TODO: REMOVE APOLLO ID BY STRINGS PROBLEM!!!
+  id: number
 }
 
