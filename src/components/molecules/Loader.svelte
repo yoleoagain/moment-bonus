65637367f57975308b0b3cc31dc574b7bba43d73
yoleo@yandex.ru <yoleo@yandex.ru>
2021-10-27 16:30:32 +0200

    Loader

---
+++ src/components/molecules/Loader.svelte
@@ -0,0 +1,20 @@
+﻿<script lang="ts">
+  import ColumnCenter from '../atoms/ColumnCenter.svelte'
+  import { Wave } from 'svelte-loading-spinners'
+  import { getContext } from 'svelte'
+
+  const { theme } = getContext('theme')
+</script>
+
+<ColumnCenter>
+  <Wave size="30" color={$theme.palette.highlitsColor} unit="px" />
+  <div class="sp">
+    <Wave size="30" class="sp" />
+  </div>
+</ColumnCenter>
+
+<style>
+  .sp {
+    transform: rotate(180deg);
+  }
+</style>
